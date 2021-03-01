27280e40d6668e7817e9656ca7407c98169618b3
pjamason <pjamason@ucsd.edu>
2021-02-28 19:10:34 -0800

    test upload worked

--- nbgrader_to_canvas/upload_grades.py
+++ nbgrader_to_canvas/upload_grades.py
@@ -5,7 +5,6 @@ from .utils import get_canvas, error
 from . import app
 from . import settings
 
-
 upload_grades_blueprint = Blueprint('upload_grades', __name__)
 
 # Web Views / Routes
@@ -18,25 +17,45 @@ def upload_grades(lti=lti):
     import canvasapi
     import pytest
     # Import the Canvas class
-    from canvasapi.submission import GroupedSubmission, Submission
-    from canvasapi.upload import Uploader
     from canvasapi.assignment import (
         Assignment,
         AssignmentGroup,
         AssignmentOverride,
         AssignmentExtension,
     )
-    from canvasapi.group import Group, GroupCategory, GroupMembership
+    from canvasapi.progress import Progress
+    from canvasapi.course import Course
 
     # Cool, we got through
     args = request.args.to_dict()
 
+    #session['course_id'] = args['course_id']
+    course_id = session['course_id']
+
     # initialize a new canvasapi Canvas object
     canvas = get_canvas()
 
-    if canvas is None:
-        courses = None
-    else:
-        courses = canvas.get_courses()
+    # to do error handling
+    #if canvas is None:
+    #    courses = None
+    #else:
+    #    courses = canvas.get_courses()
+    course = canvas.get_course(course_id)
+    NBGRADER_ASSIGN1_ID = 277845 # canvas "nbgrader assign 1"
+    TESTACCT3_USER_ID = 115753
     
-    return render_template('upload_grades.htm.j2', BASE_URL=settings.BASE_URL)
+    assignment = course.get_assignment(NBGRADER_ASSIGN1_ID)
+
+    # ASK about assignment submission type (external tool)
+
+    # TODO get assignments from canvas first
+    # if there is an nbgrader assignment id that is not in canvas,
+    # what do we use as id?
+    progress = assignment.submissions_bulk_update(
+            grade_data={TESTACCT3_USER_ID: {"posted_grade": 97}}
+        )
+
+    progress = progress.query()
+    #self.assertTrue(progress.context_type == "Course")
+    
+    return render_template('upload_grades.htm.j2', progress=progress, BASE_URL=settings.BASE_URL)
