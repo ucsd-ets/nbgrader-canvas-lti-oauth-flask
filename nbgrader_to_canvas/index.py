be3d6ed0f75d8949f115a68368a3dda45bf2433a
pjamason <pjamason@ucsd.edu>
2021-03-03 21:22:24 -0800

    500 error on upload nb grades test

--- nbgrader_to_canvas/index.py
+++ nbgrader_to_canvas/index.py
@@ -4,7 +4,6 @@ from pylti.flask import lti
 from .utils import get_canvas, error
 from . import app
 from . import settings
-from nbgrader.api import Gradebook, MissingEntry
 
 
 index_blueprint = Blueprint('index', __name__)
@@ -36,66 +35,16 @@ def index(course_id=None, user_id=None, lti=lti):
     session['user_id'] = args['user_id']
     msg = "hi! Course ID is {}, User ID is {}.".format(session['course_id'], session['user_id'])
 
-    #
-    # nbgrader code
-    #
 
-    with Gradebook("sqlite:///../gradebook.db") as gb:
-
-        # create a list of dictionaries for each submission
-        nb_assignments = gb.assignments
-        nbgraderdata = {}
-
-
-        # create a list of assignments or students to grade
-        # if lists are empty, all students are selected
-        # NEED to find a way to populate this - most likely on flask application?? #
-        assignmentList = []
-        studentList = []
-
-        # loop over all assignments
-        for assignment in gb.assignments:
-            grade_data = {}
-
-            # only continue if assignment is required
-            if assignmentList and assignment.name not in assignmentList:
-                continue
-
-            # loop over each student
-            for student in gb.students:
-
-                # only continue if student is required
-                if studentList and student.id not in studentList:
-                    continue
-
-                # ceate dict for grade_data, with nested dict {student id: {score}}
-                student_and_score = {}            
-
-                # Try to find the submission in the database. If it doesn't exist, the
-                # MissingEntry exception will be raised and we assign them a score of None.
-
-                try:
-                    submission = gb.find_submission(assignment.name, student.id)
-                except MissingEntry:
-                    student_and_score['posted_grade'] = None
-                else:
-                    student_and_score["posted_grade"] = submission.score
-
-                # student.id will give us student's username, ie shrakibullah. we will need to compare this to
-                # canvas's login_id instead of user_id
-                grade_data[student.id] = student_and_score
-
-            nbgraderdata[assignment] = grade_data
-
-
-
-
-    # initialize a new canvasapi Canvas object
+    # get canvas assignments
     canvas = get_canvas()
 
     if canvas is None:
         courses = None
     else:
         courses = canvas.get_courses()
+
+    course_id = session['course_id']
+    course = canvas.get_course(course_id)
     
-    return render_template('index.htm.j2', nbgraderdata=nbgraderdata, msg=msg, course_id=session['course_id'],all_courses=courses, BASE_URL=settings.BASE_URL)
+    return render_template('index.htm.j2', msg=msg, course_id=session['course_id'],all_courses=courses, BASE_URL=settings.BASE_URL)
--- nbgrader_to_canvas/upload_grades.py
+++ nbgrader_to_canvas/upload_grades.py
@@ -4,7 +4,9 @@ from pylti.flask import lti
 from .utils import get_canvas, error
 from . import app
 from . import settings
+from nbgrader.api import Gradebook, MissingEntry
 
+import json
 upload_grades_blueprint = Blueprint('upload_grades', __name__)
 
 # Web Views / Routes
@@ -29,7 +31,6 @@ def upload_grades(lti=lti):
     # TODO: modify below to redirect to status page after POST, see
     # https://stackoverflow.com/questions/31542243/redirect-to-other-view-after-submitting-form
 
-    # Cool, we got through
     args = request.args.to_dict()
 
     course_id = session['course_id']
@@ -42,22 +43,135 @@ def upload_grades(lti=lti):
     #    courses = None
     #else:
     #    courses = canvas.get_courses()
-    course = canvas.get_course(course_id)
-    NBGRADER_ASSIGN1_ID = 277845 # canvas "nbgrader assign 1"
-    TESTACCT3_USER_ID = 115753
-    
-    assignment = course.get_assignment(NBGRADER_ASSIGN1_ID)
-
+    #course = canvas.get_course(course_id)
+    #NBGRADER_ASSIGN1_ID = 277845 # canvas "nbgrader assign 1"
+    #TESTACCT3_USER_ID = 115753    
+    #assignment = course.get_assignment(NBGRADER_ASSIGN1_ID)
     # ASK about assignment submission type (external tool)
-
     # TODO get assignments from canvas first
     # if there is an nbgrader assignment id that is not in canvas,
     # what do we use as id?
-    progress = assignment.submissions_bulk_update(
-            grade_data={TESTACCT3_USER_ID: {"posted_grade": 97}}
-        )
+    #progress = assignment.submissions_bulk_update(
+    #        grade_data={TESTACCT3_USER_ID: {"posted_grade": 97}}
+    #    )
+    #progress = progress.query()
 
-    progress = progress.query()
-    #self.assertTrue(progress.context_type == "Course")
+    course = canvas.get_course(course_id)
+    canvas_assignments = course.get_assignments()
+    canvas_users = course.get_users()
+    
+    canvas_students = {}
+    for canvas_user in canvas_users:
+        if hasattr(canvas_user, "login_id") and canvas_user.login_id is not None:
+            canvas_students[canvas_user.login_id]=canvas_user.id
+
+            app.logger.info("canvas user login id:")
+            app.logger.info(canvas_user.login_id)    
+            app.logger.info("canvas user id:")
+            app.logger.info(canvas_user.id)        
+
+    #
+    # nbgrader code
+    #
+
+    with Gradebook("sqlite:////mnt/nbgrader/TEST_NBGRADER/grader/gradebook.db") as gb:
+
+        # create a list of dictionaries for each submission
+        nb_assignments = gb.assignments
+        nb_students = gb.students
+        nbgraderdata = {}
+
+        # create a list of assignments or students to grade
+        # if lists are empty, all students are selected
+        # NEED to find a way to populate this - most likely on flask application?? #
+        nb_assignmentList = []
+        nb_studentList = []
+
+        # loop over all nb assignments
+        for nb_assignment in nb_assignments:
+
+            app.logger.info("nb assignment name:")
+            app.logger.info(nb_assignment.name)
+            
+            nb_grade_data = {}
+
+            # only continue if nb assignment is required (pj: ?)
+            if nb_assignmentList and nb_assignment.name not in nb_assignmentList:
+                continue
+
+            # loop over each nb student
+            for nb_student in nb_students:
+
+                app.logger.info("student id:")
+                app.logger.info(nb_student.id)
+
+                # only continue if student is required (pj: ?)
+                if nb_studentList and nb_student.id not in nb_studentList:
+                    continue
+
+                # ceate dict for grade_data, with nested dict {student id: {score}}
+                nb_student_and_score = {}            
+
+                # Try to find the submission in the (pj: nbgrader?) database. If it doesn't exist, the
+                # MissingEntry exception will be raised and we assign them a score of None.
+
+                try:
+                    nb_submission = gb.find_submission(nb_assignment.name, nb_student.id)
+                except MissingEntry:
+                    nb_student_and_score['posted_grade'] = None
+                else:
+                    nb_student_and_score["posted_grade"] = nb_submission.score
+                    app.logger.info("sub name:")
+                    app.logger.info(nb_submission.name)
+                    app.logger.info("sub id:")
+                    app.logger.info(nb_submission.id)
+                    app.logger.info("sub score:")
+                    app.logger.info(nb_submission.score)
+
+                # student.id will give us student's username, ie shrakibullah. we will need to compare this to
+                # canvas's login_id instead of user_id
+
+                # convert nbgrader username to canvas id (integer)
+                #nb_grade_data[nb_student.id] = nb_student_and_score
+                canvas_student_id = canvas_students[nb_student.id]
+                nb_grade_data[canvas_student_id] = nb_student_and_score
+
+            nbgraderdata[nb_assignment] = nb_grade_data
+            match = False
+
+            # loop over canvas assignments, upload submissions if name matches
+            # TODO: check for dup assignment names
+            for canvas_assignment in canvas_assignments:
+                if nb_assignment.name == canvas_assignment.name:
+                    app.logger.info("canvas assignment name match; id:")
+                    app.logger.info(canvas_assignment.id)
+                    app.logger.info("canvas assignment name match; name:")
+                    app.logger.info(canvas_assignment.name)
+                    app.logger.info("upload submissions for existing canvas assignment")
+
+                    json_obj = json.dumps(nbgraderdata[nb_assignment])
+                    app.logger.info("json:")
+                    app.logger.info(json_obj)
+
+                    
+                    #assignment_to_upload = course.get_assignment(canvas_assignment.id)
+                    #progress = assignment_to_upload.submissions_bulk_update(grade_data=json_obj)
+                    #progress = progress.query()
+
+                    # note we found a match, exit loop
+                    match = True
+                    break
+            
+            # no match found - assignment does not exist in canvas, upload submissions
+            if match == False and nb_assignment.name == "Test Assignment 2":
+                app.logger.info("upload submissions for non-existing canvas assignment; will be named:")
+                app.logger.info(nb_assignment.name)                
+
+                json_obj = json.dumps(nbgraderdata[nb_assignment])
+                app.logger.info("json:")
+                app.logger.info(json_obj)
+                assignment_to_upload = course.get_assignment(canvas_assignment.id)
+                progress = assignment_to_upload.submissions_bulk_update(grade_data=json_obj)
+                progress = progress.query()
     
     return render_template('upload_grades.htm.j2', progress=progress, BASE_URL=settings.BASE_URL)
