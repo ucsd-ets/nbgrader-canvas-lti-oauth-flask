15bb297e787124c6da44dbc541321d80f9bcae95
pjamason <pjamason@ucsd.edu>
2021-03-23 13:43:50 -0700

    use psql sessions

--- nbgrader_to_canvas/models.py
+++ nbgrader_to_canvas/models.py
@@ -18,3 +18,15 @@ class Users(db.Model):
     def __repr__(self):
         return '<User %r>' % self.user_id
     
+class Sessions(db.Model):
+        #__tablename__ = table
+
+        id = db.Column(db.Integer, primary_key=True)
+        session_id = db.Column(db.String(255), unique=True)
+        data = db.Column(db.LargeBinary)
+        expiry = db.Column(db.DateTime)
+
+        def __init__(self, session_id, data, expiry):
+            self.session_id = session_id
+            self.data = data
+            self.expiry = expiry
